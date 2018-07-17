11f9269963d5e857420e82454546ae5b2cff6117
Song <yousonghou@yahoo.com>
2018-07-17 19:00:38 +0000

    add logInHandler logic

--- src/containers/LogIn/LogIn.js
+++ src/containers/LogIn/LogIn.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from '../../axios-lists';
 
 import { Link } from 'react-router-dom';
 
@@ -18,6 +19,33 @@ class LogIn extends Component{
         this.setState({[e.target.name]: e.target.value});
     }
     
+    logInHandler = (event) => {
+        event.preventDefault();
+        const authData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCSAapw9FhwobTVoCrIX91W-LvYzIyn4VU', authData)
+            .then(res => {
+                const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
+                
+                localStorage.setItem('token', res.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', res.data.localId);
+                
+                console.log("success" + res.data.localId);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        
+        console.log(authData);
+            
+        this.setState({username: ''});
+        this.setState({password: ''});
+    }
+    
     render() {
         return (
             <div>
@@ -33,7 +61,7 @@ class LogIn extends Component{
                         name="password"
                         value={this.state.password}
                         onChange={this.onChange} />
-                    <Button clicked={this.signUpHandler}>Log In</Button>
+                    <Button clicked={this.logInHandler}>Log In</Button>
                 </form>
                 <Link style={{display: 'block', textAlign: 'center'}} to="/signUp">
                     Sign Up
