fcdda425da93a2098788eb6f15c27d99463c7989
Song <yousonghou@yahoo.com>
2018-08-09 21:53:09 +0000

    remove connect from home and change the color of the button to pink

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import classes from './Home.css';
-import * as actionCreators from '../../store/actions/index';
 import shoppingImage from '../../assets/Images/shopping.png';
 import Button from '../../components/UI/Button/Button';
 
@@ -42,16 +40,4 @@ class Home extends Component{
     }
 }
 
-const mapStateToProps = state => {
-    return{
-        items: state.record.items
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return{
-        onStoreItem: (item) => dispatch(actionCreators.storeItem(item))
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
